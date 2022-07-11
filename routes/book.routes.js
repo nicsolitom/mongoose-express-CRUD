2ef0e81b1d7e3792dc00f2cf372041c8d7b8d146
Nic Solitom <nicsolitom@gmail.com>
2022-07-11 17:06:06 +0200

    Added: update book

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -23,7 +23,7 @@ router.get("/books/create", (req, res) => {
 });
 
 router.post("/books/create", (req, res) => {
-  console.log(req.body.title);
+  // console.log(req.body.title);
   const bookDetails = {
     title: req.body.title,
     author: req.body.author,
@@ -40,6 +40,44 @@ router.post("/books/create", (req, res) => {
     })
 
 });
+
+router.get("/books/:bookId/edit", (req, res) => {
+  const {bookId} = req.params;
+
+  Book.findById(bookId)
+    .then( (bookDetails) => {
+      res.render("books/book-edit", bookDetails);
+    })
+    .catch( (error) => {
+      console.log("Error getting book details from DB", error);
+      next(error);
+    })
+
+});
+
+router.post("/books/:bookId/edit", (req, res) => {
+  const bookId = req.params.bookId;
+
+  const newBookDetails = {
+    title: req.body.title,
+    author: req.body.author,
+    description: req.body.description,
+    rating: req.body.rating
+  }
+  console.log(newBookDetails)
+
+  Book.findByIdAndUpdate(bookId, newBookDetails)
+  .then( () => {
+    // res.redirect("/books");
+    res.redirect(`/books/${bookId}`); // redirect to book details page
+  })
+  .catch( (error) => {
+    console.log("Error updating book in DB", error);
+    next(error);
+  })
+});
+
+
 
 router.get("/books/:bookId", (req, res, next) => {
   const bookId = req.params.bookId;
