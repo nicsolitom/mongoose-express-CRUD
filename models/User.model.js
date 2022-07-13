76a4076cf46f81aec4fe836ca5e4e02863f7728f
Nic Solitom <nicsolitom@gmail.com>
2022-07-13 15:14:58 +0200

    Examples of server side validtation

--- models/User.model.js
+++ models/User.model.js
@@ -1,19 +1,23 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    username: {
+    email: {
       type: String,
-      unique: true
+      required: [true, 'Email is required.'],
+      match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
+      unique: true,
+      lowercase: true,
+      trim: true
     },
-    password: String,
+    passwordHash: {
+      type: String,
+      required: [true, 'Password is required.']
+    }
   },
   {
-    timestamps: true,
+    timestamps: true
   }
 );
 
-const User = model("User", userSchema);
-
-module.exports = User;
+module.exports = model('User', userSchema);
---
+++ routes/author.routes copy.js
@@ -0,0 +1,34 @@
+const Book = require("../models/Book.model");
+const Author = require("../models/Author.model");
+
+const router = require("express").Router();
+
+router.get("/authors", (req, res) => {
+  let books; // we create a variable in the parent scope
+
+  Book.find()
+    .then((booksFromDB) => {
+      books = booksFromDB; // update our variable in the parent scope
+      return Author.find()
+    })
+    .then(authorsFromDB => {
+
+      const authorsArr = authorsFromDB.map((authorDetails) => {
+        const { _id, name, age, country } = authorDetails;
+        const booksFromCurrentAuthor = books.filter(book => {
+          return book.author.toString() === _id.toString(); // compare ids as string
+        })
+        const numberOfBooks = booksFromCurrentAuthor.length;
+
+        return { name, age, country, numberOfBooks };
+      });
+
+      res.render("authors/authors-list", { authorsArr });
+    })
+    .catch((error) => {
+      console.log("Error getting authors from DB", error);
+      next(error);
+    })
+});
+
+module.exports = router;
