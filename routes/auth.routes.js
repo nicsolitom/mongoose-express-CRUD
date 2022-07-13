7a273c13cfaf75bbaedc62511561c5f049a0927b
Nic Solitom <nicsolitom@gmail.com>
2022-07-13 16:51:20 +0200

    sessions and session persistance (connect-mongo)

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -86,9 +86,8 @@ router.post("/login", (req, res, next) => {
         return;
       } else if (bcryptjs.compareSync(password, userFromDB.passwordHash)) {
         //login sucessful
-        // req.session.currentUser = userFromDB;
-        // res.redirect("/user-profile");
-        res.render('auth/user-profile', {user: userFromDB});
+        req.session.currentUser = userFromDB;
+        res.redirect("/user-profile");
       } else {
         //login failed (password doesn't match)
         res.render('auth/login', { errorMessage: 'Incorrect credentials.' });
@@ -105,7 +104,12 @@ router.post("/login", (req, res, next) => {
 
 // Profile page
 router.get('/user-profile', (req, res, next) => {
-  res.render('auth/user-profile');
+
+  if(req.session.currentUser) {
+    console.log("it seems like the user is logged in .....")
+  }
+
+  res.render('auth/user-profile', {user: req.session.currentUser});
 });
 
 module.exports = router;
