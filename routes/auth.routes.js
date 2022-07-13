e0d98be8ce2da63a2da0afadf5bdadcd6e617023
Nic Solitom <nicsolitom@gmail.com>
2022-07-13 17:04:51 +0200

    functionality to logout

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -99,7 +99,13 @@ router.post("/login", (req, res, next) => {
     });
 })
 
-
+// Logout
+router.post('/logout', (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) next(err);
+    res.redirect('/');
+  });
+});
 
 
 // Profile page
